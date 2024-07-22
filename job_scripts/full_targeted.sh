9d343921dba3a7d3c9bd33d714f445667e1c0d82
Forrest Hutchison <forrest.hutchison@yale.edu>
2024-07-22 15:08:15 -0500

    job script

---
+++ job_scripts/full_targeted.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=t001
+#SBATCH --time=3-00:00:00
+#SBATCH --partition=pi_mingarelli
+#SBATCH --output=logs/target_001_det.txt
+#SBATCH --error=logs/error/target_001_det.txt
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=4
+#SBATCH --nodes=1
+#SBATCH --mem-per-cpu=10G
+
+module load miniconda
+module load OpenMPI
+conda activate targeted
+
+export PYTHONPATH=$(pwd):$PYTHONPATH
+python scripts/full_targeted.py
