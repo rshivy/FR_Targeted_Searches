c64737075572bcae554786f9edcad12c890c2b06
Forrest Hutchison <forrest.hutchison@yale.edu>
2024-07-26 14:55:00 -0400

    update job scripts

--- job_scripts/full_targeted_pt_m.sh
+++ job_scripts/full_targeted_pt_m.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name=t001
 #SBATCH --time=3-00:00:00
 #SBATCH --partition=pi_mingarelli
-#SBATCH --output=logs/target_001_det_narrowfgw.txt
-#SBATCH --error=logs/error/target_001_det_narrowfgw.txt
+#SBATCH --output=logs/target_001_det_narrowfgw_pt_m.txt
+#SBATCH --error=logs/error/target_001_det_narrowfgw_pt_m.txt
 #SBATCH --cpus-per-task=1
-#SBATCH --ntasks=1
+#SBATCH --ntasks=4
 #SBATCH --nodes=1
 #SBATCH --mem-per-cpu=10G
 
@@ -14,4 +14,4 @@ module load OpenMPI
 conda activate targeted
 
 export PYTHONPATH=$(pwd):$PYTHONPATH
-python scripts/full_targeted.py
+mpirun -n 4 python scripts/full_targeted.py
