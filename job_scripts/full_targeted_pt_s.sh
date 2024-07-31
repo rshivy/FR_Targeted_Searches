79caaa2d71bd7174b812d3d2a1f824074ee2b7d8
Forrest Hutchison <forrest.hutchison@yale.edu>
2024-07-31 14:31:26 -0400

    prep new pt run

--- job_scripts/full_targeted_pt_s.sh
+++ job_scripts/full_targeted_pt_s.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-#SBATCH --job-name=t001
+#SBATCH --job-name=t001pt
 #SBATCH --time=3-00:00:00
 #SBATCH --partition=pi_mingarelli
 #SBATCH --output=logs/target_001_det_narrowfgw_pt_s.txt
 #SBATCH --error=logs/error/target_001_det_narrowfgw_pt_s.txt
 #SBATCH --cpus-per-task=1
-#SBATCH --ntasks=4
+#SBATCH --ntasks=8
 #SBATCH --nodes=1
 #SBATCH --mem-per-cpu=10G
 
