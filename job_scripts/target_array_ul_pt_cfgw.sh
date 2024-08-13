08ec488463b487c4aabfe3029b08b0d956dfc4d5
Forrest Hutchison <forrest.hutchison@yale.edu>
2024-08-13 10:53:43 -0400

    fix job script

--- job_scripts/target_array_ul_pt_cfgw.sh
+++ job_scripts/target_array_ul_pt_cfgw.sh
@@ -3,8 +3,8 @@
 #SBATCH --time=1-00:00:00
 #SBATCH --partition=scavenge
 #SBATCH --array=1,3,5,7,11,13,15,17,19,21
-#SBATCH --output=logs/target_%3a_det_cfgw_pt_v3.txt
-#SBATCH --error=logs/error/target_%3a_det_cfgw_pt_v3.txt
+#SBATCH --output=logs/target_%3a_ul_cfgw_pt_v3.txt
+#SBATCH --error=logs/error/target_%3a_ul_cfgw_pt_v3.txt
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks=8
 #SBATCH --nodes=1
@@ -16,4 +16,4 @@ module load OpenMPI
 conda activate targeted
 
 export PYTHONPATH=$(pwd):$PATH
-srun -n 8 python scripts/full_targeted.py -t $SLURM_ARRAY_TASK_ID -m detection -f constant -o cfgw_pt_v3
+srun -n 8 python scripts/full_targeted.py -t $SLURM_ARRAY_TASK_ID -m upper-limit -f constant -o cfgw_pt_v3
