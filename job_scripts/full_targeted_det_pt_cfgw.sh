9acb21faa0d227e9fffe942b43fe687c5026fa90
Forrest Hutchison <forrest.hutchison@yale.edu>
2024-11-17 20:53:01 -0500

    cfgw detections single target

---
+++ job_scripts/full_targeted_det_pt_cfgw.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=201-det-cfgw
+#SBATCH --time=5-00:00:00
+#SBATCH --partition=pi_mingarelli
+#SBATCH --output=logs/target_201_det_cfgw_pt_v3.txt
+#SBATCH --error=logs/error/target_201_det_cfgw_pt_v3.txt
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=8
+#SBATCH --nodes=1
+#SBATCH --mem-per-cpu=10G
+
+module load miniconda
+module load OpenMPI
+conda activate targeted
+
+export PYTHONPATH=$(pwd):$PYTHONPATH
+srun -n 8 python scripts/full_targeted.py -o pt_v3 -t 201 -m detection -f constant
